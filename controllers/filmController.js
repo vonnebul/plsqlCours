6d9fc90fd1e1e5659f9c298f78efa8abd431911a
Vonnebul <vonnebul@gmail.com>
2024-11-26 11:21:42 +0100

    checking

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -7,12 +7,3 @@ exports.getFilm = async(req, res) =>{
     res.status(200).json(rows)
 };
 
-exports.getFilmPS = async(req, res) =>{
-    let conn;
-    conn = await pool.getConnection();
-    const rows = await conn.query('CALL get_all_films();')
-    const convertedRows = JSON.parse(JSON.stringify(rows, (key, value) =>
-        typeof value === 'bigint' ? value.toString() : value
-    ));
-    res.status(200).json(convertedRows)
-};
--- routes/filmRoute.js
+++ routes/filmRoute.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const filmController = require('../controllers/filmController');
 
 router.get('/', filmController.getFilm);
-router.get('/ps', filmController.getFilmPS);
 
 module.exports = router;
